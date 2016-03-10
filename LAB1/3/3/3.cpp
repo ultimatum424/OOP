648311c951fd20d2630ec039258247a5d636a11e
ultimatum424 <ultimatum424@yandex.ru>
2016-03-10 16:46:18 +0300

    LAB 1-3 Умножение матриц (Рефакторинг кода)

--- LAB1/3/3/3.cpp
+++ LAB1/3/3/3.cpp
@@ -11,82 +11,78 @@ using namespace std;
 
 const int DIMENSION = 3;
 
-int ReadFile(ifstream& input_file, float  martix[DIMENSION][DIMENSION])
+bool ReadFromFile(ifstream& inputFile, float  martix[DIMENSION][DIMENSION])
 {
-	while (!input_file.eof())
+	while (!inputFile.eof())
 	{
-		for (int i = 0; i < DIMENSION; i++)
+		for (size_t i = 0; i < DIMENSION; i++)
 		{
-			for (int j = 0; j < DIMENSION; j++)
+			for (size_t j = 0; j < DIMENSION; j++)
 			{
-				string check_string;
-				input_file >> check_string;
-				martix[i][j] = atof(check_string.c_str());
-				if (((martix[i][j] == 0) && (check_string != "0")))
+				string read;
+				inputFile >> read;
+				martix[i][j] = atof(read.c_str());
+				if (((martix[i][j] == 0) && (read != "0")))
 				{
-					cout << "!!!!!";
-					input_file.close();
-					return 1;
+					inputFile.close();
+					return false;
 				}
 			}
 		}
 	}
-	input_file.close();
-	return 0;
+	inputFile.close();
+	return true;
 }
-void MultiplicationMatrix(float  martix1[DIMENSION][DIMENSION], float  martix2[DIMENSION][DIMENSION], float  result_martix[DIMENSION][DIMENSION])
+void MultiplicationMatrix(float martix1[DIMENSION][DIMENSION], float martix2[DIMENSION][DIMENSION], float resultMartix[DIMENSION][DIMENSION])
 {
-	result_martix[0][0] = martix1[0][0] * martix2[0][0] + martix1[0][1] * martix2[1][0] + martix1[0][2] * martix2[2][0];
-	result_martix[0][1] = martix1[0][0] * martix2[0][1] + martix1[0][1] * martix2[1][1] + martix1[0][2] * martix2[2][1];
-	result_martix[0][2] = martix1[0][0] * martix2[0][2] + martix1[0][1] * martix2[1][2] + martix1[0][2] * martix2[2][2];
+	resultMartix[0][0] = martix1[0][0] * martix2[0][0] + martix1[0][1] * martix2[1][0] + martix1[0][2] * martix2[2][0];
+	resultMartix[0][1] = martix1[0][0] * martix2[0][1] + martix1[0][1] * martix2[1][1] + martix1[0][2] * martix2[2][1];
+	resultMartix[0][2] = martix1[0][0] * martix2[0][2] + martix1[0][1] * martix2[1][2] + martix1[0][2] * martix2[2][2];
 
-	result_martix[1][0] = martix1[1][0] * martix2[0][0] + martix1[1][1] * martix2[1][0] + martix1[1][2] * martix2[2][0];
-	result_martix[1][1] = martix1[1][0] * martix2[0][1] + martix1[1][1] * martix2[1][1] + martix1[1][2] * martix2[2][1];
-	result_martix[1][2] = martix1[1][0] * martix2[0][2] + martix1[1][1] * martix2[1][2] + martix1[1][2] * martix2[2][2];
+	resultMartix[1][0] = martix1[1][0] * martix2[0][0] + martix1[1][1] * martix2[1][0] + martix1[1][2] * martix2[2][0];
+	resultMartix[1][1] = martix1[1][0] * martix2[0][1] + martix1[1][1] * martix2[1][1] + martix1[1][2] * martix2[2][1];
+	resultMartix[1][2] = martix1[1][0] * martix2[0][2] + martix1[1][1] * martix2[1][2] + martix1[1][2] * martix2[2][2];
 
-	result_martix[2][0] = martix1[2][0] * martix2[0][0] + martix1[2][1] * martix2[1][0] + martix1[2][2] * martix2[2][0];
-	result_martix[2][1] = martix1[2][0] * martix2[0][1] + martix1[2][1] * martix2[1][1] + martix1[2][2] * martix2[2][1];
-	result_martix[2][2] = martix1[2][0] * martix2[0][2] + martix1[2][1] * martix2[1][2] + martix1[2][2] * martix2[2][2];
+	resultMartix[2][0] = martix1[2][0] * martix2[0][0] + martix1[2][1] * martix2[1][0] + martix1[2][2] * martix2[2][0];
+	resultMartix[2][1] = martix1[2][0] * martix2[0][1] + martix1[2][1] * martix2[1][1] + martix1[2][2] * martix2[2][1];
+	resultMartix[2][2] = martix1[2][0] * martix2[0][2] + martix1[2][1] * martix2[1][2] + martix1[2][2] * martix2[2][2];
 }
 
 int main(int argc, char* argv[])
 {
 	if (argc != 3)
 	{
-		cout << "wrong number of arguments" << endl << "Enter the files 1matrix, 2matrix " << endl;
+		cout << "wrong number of arguments" << endl << "Usage: <first matrix file> <second matrix file>" << endl;
 		return 1;
 	}
 	else
 	{
-		ifstream input_file1;
-		ifstream input_file2;
-		ofstream out_file;
-		input_file1.open(argv[1]);
-		input_file2.open(argv[2]);
-		if ((input_file1.is_open()) && (input_file2.is_open()))
+		ifstream inputFile1;
+		ifstream inputFile2;
+		ofstream outFile;
+		inputFile1.open(argv[1]);
+		inputFile2.open(argv[2]);
+		if ((inputFile1.is_open()) && (inputFile2.is_open()))
 		{
 			float  martix1[DIMENSION][DIMENSION];
 			float  martix2[DIMENSION][DIMENSION];
-			float  result_martix[DIMENSION][DIMENSION];
+			float  resultMartix[DIMENSION][DIMENSION];
 			
-			bool check_correctness_matrix1 = ReadFile(input_file1, martix1);
-			bool check_correctness_matrix2 = ReadFile(input_file2, martix2);
-			if ((check_correctness_matrix1) || (check_correctness_matrix2))
+			if ((!ReadFromFile(inputFile1, martix1)) || (!ReadFromFile(inputFile2, martix2)))
 			{
-				cout << "matrix format is not a 3X3";
+				cout << "dimension of matrix is not a 3X3";
 				return 1;
 			}
-			cout << "!!!!!!!!!";
-			MultiplicationMatrix(martix1, martix2, result_martix);
-			out_file.open("result_martix.txt");
-			out_file << result_martix[0][0] << " " << result_martix[0][1] << " " << result_martix[0][2] << endl;
-			out_file << result_martix[1][0] << " " << result_martix[1][1] << " " << result_martix[1][2] << endl;
-			out_file << result_martix[2][0] << " " << result_martix[2][1] << " " << result_martix[2][2] << endl;
-			out_file.close();
+			MultiplicationMatrix(martix1, martix2, resultMartix);
+			outFile.open("result_martix.txt");
+			outFile << resultMartix[0][0] << " " << resultMartix[0][1] << " " << resultMartix[0][2] << endl;
+			outFile << resultMartix[1][0] << " " << resultMartix[1][1] << " " << resultMartix[1][2] << endl;
+			outFile << resultMartix[2][0] << " " << resultMartix[2][1] << " " << resultMartix[2][2] << endl;
+			outFile.close();
 		}
 		else
 		{
-			cout << "Error opening file " << endl;
+			cout << "Erorr: Can't open file" << endl;
 			return 1;
 		}
 	}
